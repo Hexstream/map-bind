f401a8a183d91f4bd47c72a429df45e894f7e402
Jean-Philippe Paradis <hexstream@hexstreamsoft.com>
2018-09-25 09:37:25 -0400

    Tests: Use proper argument order for IS.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -5,31 +5,27 @@
 (cl:in-package #:map-bind_tests)
 
 (define-test "mapcar, one argument, one list"
-  (is equal
+  (is equal '(2 3 4)
       (map-bind (mapcar) ((number '(1 2 3)))
-        (1+ number))
-      '(2 3 4)))
+        (1+ number))))
 
 (define-test "funcall mapcar, one argument, one list"
   :depends-on ("mapcar, one argument, one list")
-  (is equal
+  (is equal '(2 3 4)
       (map-bind (funcall #'mapcar) ((number '(1 2 3)))
-        (1+ number))
-      '(2 3 4)))
+        (1+ number))))
 
 (define-test "apply mapcar, one argument, one list"
   :depends-on ("funcall mapcar, one argument, one list")
-  (is equal
+  (is equal '(2 3 4)
       (map-bind (apply #'mapcar) ((number (list '(1 2 3))))
-        (1+ number))
-      '(2 3 4)))
+        (1+ number))))
 
 (define-test "featured-complex-example"
-  (is equalp
+  (is equalp #((0 A 11 (4 D)) (1 B 12 (5 E)) (2 C 13 (6 F)))
       (map-bind (multiple-value-call #'map 'vector)
           ((symbol #(a b c))
            (number '(1 2 3))
            ((&rest others &key &allow-other-keys) (values '(d e f) #(4 5 6)))
            ((&aux (plus-ten (+ number 10)))))
-        (list (1- number) symbol plus-ten (reverse others)))
-      #((0 A 11 (4 D)) (1 B 12 (5 E)) (2 C 13 (6 F)))))
+        (list (1- number) symbol plus-ten (reverse others)))))
